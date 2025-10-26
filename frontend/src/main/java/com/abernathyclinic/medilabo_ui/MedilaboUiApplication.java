5ea1d284f5b19edcdf09d8c7c22d1f4a670686b9
diveshkumar <06divesh@gmail.com>
2025-10-26 10:39:38 -0400

    initial

---
+++ frontend/src/main/java/com/abernathyclinic/medilabo_ui/MedilaboUiApplication.java
@@ -0,0 +1,13 @@
+package com.abernathyclinic.medilabo_ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedilaboUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedilaboUiApplication.class, args);
+	}
+
+}
---
+++ frontend/src/test/java/com/abernathyclinic/medilabo_ui/MedilaboUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abernathyclinic.medilabo_ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedilaboUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
