dd3b40863cd016fd704e2fde3ab8401cc5d24f6f
diveshkumar <06divesh@gmail.com>
2025-11-15 21:51:40 -0500

    Created Crud operations for Medilabo. Created postgres sql added pgadmin

---
+++ demographics/src/main/java/com/abernathyclinic/medilabo/controller/PatientController.java
@@ -0,0 +1,35 @@
+package com.abernathyclinic.medilabo.controller;
+
+import com.abernathyclinic.medilabo.domain.Patient;
+import com.abernathyclinic.medilabo.repository.PatientRepo;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+
+    private final PatientRepo patientRepo;
+
+    public PatientController(PatientRepo service) {
+        this.patientRepo = service;
+    }
+
+
+    @GetMapping
+    public List<Patient> getAllPatients() {
+        return patientRepo.findAll();
+    }
+
+    @PostMapping
+    public Patient createPatient(@RequestBody Patient patient) {
+        return patientRepo.save(patient);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePatient(@PathVariable Long id) {
+        patientRepo.deleteById(id);
+    }
+
+}
---
+++ demographics/src/main/java/com/abernathyclinic/medilabo/repository/PatientRepo.java
@@ -0,0 +1,13 @@
+package com.abernathyclinic.medilabo.repository;
+
+import com.abernathyclinic.medilabo.domain.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PatientRepo extends JpaRepository<Patient, Long> {
+    List<Patient> findByName(String name);
+}
+
