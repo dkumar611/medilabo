84ee9d93b99ad0053f3177039d6c03c179e8a5f7
diveshkumar <06divesh@gmail.com>
2025-12-06 22:46:22 -0500

    created new microservice - noteservice for taking medical notes

---
+++ noteservice/src/main/java/com/abernathyclinic/medilabo_noteservice/MedilaboNoteserviceApplication.java
@@ -0,0 +1,13 @@
+package com.abernathyclinic.medilabo_noteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedilaboNoteserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedilaboNoteserviceApplication.class, args);
+	}
+
+}
---
+++ noteservice/src/test/java/com/abernathyclinic/medilabo_noteservice/MedilaboNoteserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abernathyclinic.medilabo_noteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedilaboNoteserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
