d493db5f5f672f0102a5b54d2673b935edbc3ac5
diveshkumar <06divesh@gmail.com>
2026-01-01 19:45:18 -0500

    added jwt for login, added new services for diabetes assessment. everything is working

---
+++ medilabo-diabetes-assessment/src/main/java/com/abernathyclinic/medilabo_diabetes_assessment/MedilaboDiabetesAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.abernathyclinic.medilabo_diabetes_assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedilaboDiabetesAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedilaboDiabetesAssessmentApplication.class, args);
+	}
+
+}
---
+++ medilabo-diabetes-assessment/src/test/java/com/abernathyclinic/medilabo_diabetes_assessment/MedilaboDiabetesAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abernathyclinic.medilabo_diabetes_assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedilaboDiabetesAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
